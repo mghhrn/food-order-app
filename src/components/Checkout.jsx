c9e73ef77da7cd5515defe642fc3768cc103eec0
Mahdi Ghaharian <mahdighaharian@MBP-MGH.local>
2024-05-14 02:29:38 +0400

    implemented calling RESTful API to post the data to the backend

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -19,23 +19,43 @@ export default function Checkout() {
         userProgressCtx.hideCheckout();
     }
 
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const fd = new FormData(event.target);
+        const customerData = Object.fromEntries(fd.entries());
+
+        fetch('http://localhost:3000/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                order: {
+                    items: cartCtx.items,
+                    customer: customerData
+                }
+            })
+        });
+    }
+
     return (
         <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Checkout</h2>
                 <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-                <Input label="Full Name" type="text" id="full-name" />
+                <Input label="Full Name" type="text" id="name" />
                 <Input label="E-Mail Address" type="email" id="email" />
                 <Input label="Street" type="text" id="street" />
                 <div className="control-row" >
-                    <Input lable="Postal Code" type="text" id="postal-code" />
+                    <Input label="Postal Code" type="text" id="postal-code" />
                     <Input label="City" type="text" id="city" />
                 </div>
 
                 <p className="modal-actions">
                     <Button type="button" textOnly onClick={handleClose}>Close</Button>
-                    <Button textOnly>Submit Order</Button>
+                    <Button >Submit Order</Button>
                 </p>
             </form>
         </Modal>
